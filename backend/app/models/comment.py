7dba6b92914e85d5715e3fc0f6433dc5cdb1527d
Vobla1979 <korova666e@yandex.ru>
2025-12-29 17:57:22 +0300

    Update comment.py

--- backend/app/models/comment.py
+++ backend/app/models/comment.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, Text, ForeignKey
+from sqlalchemy import Column, Integer, Text, ForeignKey, DateTime, func
 from sqlalchemy.orm import relationship
 from app.core.db import Base
 
@@ -9,6 +9,8 @@ class Comment(Base):
     content = Column(Text, nullable=False)
     author_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     post_id = Column(Integer, ForeignKey("posts.id"), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
     author = relationship("User", back_populates="comments")
     post = relationship("Post", back_populates="comments")
