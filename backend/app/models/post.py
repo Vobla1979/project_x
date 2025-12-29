df019b8b6bdbaf2b45e896d8f4f864f6acc5fc87
Vobla1979 <korova666e@yandex.ru>
2025-12-29 17:49:38 +0300

    Add timestamps to post model

--- backend/app/models/post.py
+++ backend/app/models/post.py
@@ -9,6 +9,8 @@ class Post(Base):
     title = Column(String, index=True)
     content = Column(Text)
     author_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
     author = relationship("User", back_populates="posts")
     comments = relationship("Comment", back_populates="post")
