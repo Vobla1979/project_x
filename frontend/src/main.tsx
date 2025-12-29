02d2fac66c14fc982fe12a3783a78585f1fd34e8
Vobla1979 <korova666e@yandex.ru>
2025-12-29 23:18:03 +0300

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,6 +9,24 @@ import NewPostPage from "./pages/NewPostPage";
 import PostPage from "./pages/PostPage";
 import ProfilePage from "./pages/ProfilePage";
 
+const API_URL = "http://localhost:8000";
+
+async function apiRequest(path: string, options: RequestInit = {}) {
+  const token = localStorage.getItem("token");
+  const headers: HeadersInit = {
+    "Content-Type": "application/json",
+    ...(options.headers || {}),
+  };
+  if (token) headers["Authorization"] = `Bearer ${token}`;
+  
+  const resp = await fetch(`${API_URL}${path}`, { ...options, headers });
+  if (!resp.ok) {
+    const data = await resp.json().catch(() => ({}));
+    throw { status: resp.status, data };
+  }
+  return resp.json();
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
