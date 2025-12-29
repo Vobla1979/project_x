daad1e4d2a73418b5b3a8d9411d8f43368dd2cf2
Vobla1979 <korova666e@yandex.ru>
2025-12-29 18:45:02 +0300

    Initialize main.tsx with React Router setup

---
+++ frontend/src/main.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./auth/AuthContext";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import HomePage from "./pages/HomePage";
+import NewPostPage from "./pages/NewPostPage";
+import PostPage from "./pages/PostPage";
+import ProfilePage from "./pages/ProfilePage";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/posts/new" element={<NewPostPage />} />
+          <Route path="/posts/:id" element={<PostPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
