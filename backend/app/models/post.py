79e616b99d4e0bd879551d999722730ce66db5fa
Vobla1979 <korova666e@yandex.ru>
2025-12-29 17:51:14 +0300

    Add DateTime and func imports to post model

--- backend/app/models/post.py
+++ backend/app/models/post.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Text, ForeignKey
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, DateTime, func
 from sqlalchemy.orm import relationship
 from app.core.db import Base
 
